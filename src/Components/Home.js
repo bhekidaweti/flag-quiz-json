15a05bd861dd42593af2d7101b79383063f54376
bheki daweti <bheki.daweti@gmail.com>
2025-02-12 15:27:43 +0200

    added og-image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,6 +10,23 @@ const Home = () => {
             <meta name="description" content="Welcome to the Flag Quiz! Challenge yourself to identify flags from around the world." />
             <meta property="og:title" content="World Flag Quiz - Home!" />
             <meta property="og:description" content="Can you name all the flags? Start the quiz now and find out!" />
+
+                  {/* Open Graph Tags */}
+            <meta property="og:title" content="Flag Quiz - Knowledge Base!" />
+            <meta property="og:type" content="website" />
+            <meta property="og:image" content="https://funwithworldflags.com/og-image.jpg" />
+            <meta property="og:url" content="https://funwithworldflags.com/" />
+            <meta property="og:description" content="Can you name all the flags? Start the quiz now and find out!" />
+            
+            {/* Twitter Card Tags */}
+            <meta name="twitter:card" content="summary_large_image" />
+            <meta name="twitter:site" content="@d_bheki" />
+            <meta name="twitter:title" content="Flag Quiz - Knowledge Base!" />
+            <meta name="twitter:description" content="Can you name all the flags? Start the quiz now and find out!" />
+            <meta name="twitter:image" content="https://funwithworldflags.com/og-image.jpg" />
+            
+            {/* Canonical URL */}
+            <link rel="canonical" href="https://funwithworldflags.com/" />
           </Helmet>
                     <section className="welcome">
                         Welcome to the flags quiz game where you can test your world flags knowledge and also 
